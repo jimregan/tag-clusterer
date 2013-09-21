8a56ce9469e9012129702a96763ac683ddc53494
Jimmy O'Regan <joregan@gmail.com>
2013-09-21 20:27:32 +0100

    delexicaliser

---
+++ delexicalise.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+while(<>) {
+	chomp;
+	# Trim leading & tailing whitespace
+	s/^\s*//;
+	s/\s*$//;
+
+	if (/^$/) {
+		print "\n";
+		next;
+	}
+
+	# This is supposed to be one token per line,
+	# so dump the begin and end markers and check for them
+	s/^\^//;
+	s/\$$//;
+	die "Input should be one token per line" if (/[\^\$]/);
+
+	# Throw away any escaped slashes
+	s/\\\///;
+
+	# We only care about the analysis
+	my @parts = split/\//;
+	die "Untagged text, cannot continue" if ($#parts != 1);
+	$_ = $parts[1];
+
+	#And now we delexicalise (=throw anything that's not a tag)
+	s/^([^<]*)//;
+	s/>\+([^<]*)</>+</g;
+	s/([^>]*)$//;
+
+	print "$_\n";
+}
